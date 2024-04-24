22925f43267d0306de858fb61f6de2ec32a97178
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-24 15:57:37 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+    Promise.all([uploadPhoto(), createUser()])
+        .then((obj) => {
+            console.log(`${obj[0].body} ${obj[1].firstName} ${obj[1].lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
+}
+
+export default handleProfileSignup;
