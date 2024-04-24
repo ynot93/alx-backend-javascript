95af0382a308c48d0b2fe2d8f81e4b21b849af8c
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-24 21:06:50 +0300

    Fix export statement

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then(results => {
       return results.map(result => ({
