ebe47a0ba2ea05a1f71a8ab96dd66d1a4280a411
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-25 18:37:45 +0300

    Define number method of class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,43 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(setValue) {
+    if (typeof setValue !== 'number') {
+        throw new TypeError('Size must be a number');
+    }
+    this._size = setValue;
+  }
+
+  get location() {
+      return this._location;
+  }
+
+  set location(setValue) {
+      if (typeof setValue !== 'string') {
+          throw new TypeError('Location must be a string');
+      }
+      this._location = setValue;
+  }
+
+  valueOf() {
+      return this._size;
+  }
+
+  toString() {
+      return this._location;
+  }
+}
