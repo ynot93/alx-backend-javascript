1454d322cc8cc494f45ac2b667480a731fc02b6f
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-25 18:34:40 +0300

    Implement class method in Currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name !== 'string' || typeof code !== 'string') {
+      throw new TypeError('Name/Code must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._name;
+  }
+
+  set code(setCode) {
+    if (typeof setCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = setName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(setName) {
+    if (typeof setName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = setName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
