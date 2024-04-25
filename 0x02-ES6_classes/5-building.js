bf44ddc924dbcc38771082920a51e75a05687121
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-25 18:35:56 +0300

    Make an abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new Error('Sqft must be a number');
+    }
+    if (new.target === Building) {
+      throw new TypeError("This is an abstract class");
+    }
+    if (this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
