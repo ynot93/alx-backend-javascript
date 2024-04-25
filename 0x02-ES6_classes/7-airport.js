4da395d5cba4123d807601b8533c8d263cabb42d
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-25 18:37:04 +0300

    Define string description of class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'number') {
+      throw new Error('Name must be a number');
+    }
+    if (typeof code !== 'number') {
+      throw new Error('Code must be a number');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
