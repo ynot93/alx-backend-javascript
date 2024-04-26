bb032f461f7530acd3a6f3f1b8e701ff6f9ddf57
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-26 19:04:05 +0000

    Fix linting errors

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,10 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      return new Error();
-    })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
