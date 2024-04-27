ceae15c7b9864aa791093802341f0b908505c0cc
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-27 06:02:05 +0300

    Return results list

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => (
-      results.map((result) => ({
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      return results.map((result) => ({
         status: result.status,
         value: result.reason || result.value,
-      }))
-    ));
+      }));
+    });
 }
