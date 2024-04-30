15fcbd939791e6daa13674b7ee325a67e7674d7d
Tony Mputhia <tonymmputhia@gmail.com>
2024-04-30 22:41:30 +0300

    Return a string of all values that start with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string' || startString.length === 0) {
+      return '';
+  }
+
+  const filteredValues = Array.from(set)
+    .filter(item => typeof item === 'string' && item.startsWith(startString))
+    .map(item => item.substring(startString.length));
+
+  return filteredValues.join('-');
+}
