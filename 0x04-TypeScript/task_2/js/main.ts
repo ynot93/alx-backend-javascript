2881df13d090c0493338aa96eaa68665068419f7
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-01 22:01:09 +0300

    Create teacher and director interfaces and classes

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,49 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  let numericSalary = typeof salary === "string" ? parseInt(salary) : salary;
+  if (numericSalary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
