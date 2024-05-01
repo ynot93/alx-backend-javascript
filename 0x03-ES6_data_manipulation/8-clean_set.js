672c779124061ed132d7ff204eede9586ea4d756
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-01 20:29:10 +0300

    Fix linting error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || startString.length === 0) {
-      return '';
+    return '';
   }
 
   const filteredValues = Array.from(set)
