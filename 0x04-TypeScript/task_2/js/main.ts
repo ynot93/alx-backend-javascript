1791f704d44b9d37c8dc13871863102728ea1428
Tony Mputhia <tonymmputhia@gmail.com>
2024-05-01 22:08:56 +0300

    Using string literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -59,3 +59,15 @@ function executeWork(employee: Director | Teacher): string {
   }
 }
 
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  } else {
+    return "Invalid class";
+  }
+}
+
