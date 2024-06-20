5660d002a1c3d5481b67c95302cd849e8aaa9c7b
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-20 08:54:11 +0300

    Fix endpoints to work correctly

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,23 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
-});
-
-// Define a fallback route for any other endpoint
-app.use((req, res) => {
-  res.status(404).send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="utf-8">
-        <title>Error</title>
-        </head>
-        <body>
-        <pre>Cannot GET ${req.originalUrl}</pre>
-        </body>
-        </html>
-    `);
+  res.send('Hello Holberton School!');
 });
 
 app.listen(1245, () => {
