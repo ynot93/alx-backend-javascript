c49493f0c83bbac2d161b77e4c87549aca824084
Tony Mputhia <tonymmputhia@gmail.com>
2024-06-20 16:03:10 +0300

    Test async progrms with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  });
+});
